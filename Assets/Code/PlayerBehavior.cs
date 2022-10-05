64c35ef3290564f50a8ca3bc4b51ebafc3d9bdf8
evroman03 <evroman@mail.bradley.edu>
2022-10-05 16:28:22 -0500

    MoveWorkingNow

--- Assets/Code/PlayerBehavior.cs
+++ Assets/Code/PlayerBehavior.cs
@@ -1,34 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
-    public float userSpeed = 5;
-    public Vector2 jumpMag = new Vector2(0, 150);
+    public float UserSpeed = 8;
+    public float Timer;
+    public Vector2 jumpMag = new Vector2(0, 1200);
 
     private Rigidbody2D rb2D;
 
+    bool JumpTimeout = false;
 
 
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * userSpeed * Time.deltaTime);
+            transform.Translate(Vector3.right * UserSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * userSpeed * Time.deltaTime);
+            transform.Translate(Vector3.left * UserSpeed * Time.deltaTime);
         }
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W) && !JumpTimeout)
         {
-            
+            rb2D.velocity = Vector2.zero;
             rb2D.AddForce(jumpMag);
+            JumpTimeout = true;
+            Timer = .55f;
+        }
+        if(JumpTimeout == true)
+            {
+               
+                Timer = Timer - Time.deltaTime;
+                if (Timer <= 0)
+                {
+                    JumpTimeout = false;
+                }
+            }
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "platforms")
+        {
+            print("shit");
         }
     }
-
 
 
 
